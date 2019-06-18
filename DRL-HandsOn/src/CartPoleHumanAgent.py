d074e6ceae2e5871e6e950fd1cb8c7601ee5db56
Serhuela <37186336+serhuela@users.noreply.github.com>
2019-06-18 18:54:55 +0200

    first nn done. monitorized done.

---
+++ DRL-HandsOn/src/CartPoleHumanAgent.py
@@ -0,0 +1,23 @@
+import gym
+
+# TODO: Develop this human agent
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v0")
+
+    total_reward = 0.0
+    total_steps = 0
+    obs = env.reset()
+    env.render()
+
+    while True:
+        action = env.action_space.sample()
+        obs, reward, done, _ = env.step(action)
+        env.render()
+        total_reward += reward
+        total_steps += 1
+        if done:
+            env.close()
+            break
+
+    print("Episode done in %d steps, total reward %.2f" % (total_steps, total_reward))
